c0d372d24372d163c1be0ecc40bcbd8e7ac75f5f
carmine ingaldi <ingaldi.csb@gmail.com>
2017-06-25 13:04:37 +0200

    added tests directory for better code organization

---
+++ tests/tests.js
@@ -0,0 +1,21 @@
+/**
+ *  Unit tests root file
+ *  
+ *  TODO: in complex app design, would be nice to create a 'test' folder 
+ *  that reflects the development source tree, so for each module 'app/subdir/mymodule'
+ *  there would be a 'test/subdir/mymodule_tests'. This keeps clean project organization
+ *   
+ */
+
+
+var chai = require ('chai');
+
+
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should always fail', function(){
+         chai.assert.equal(1 , 2);
+    });
+  });
+});
